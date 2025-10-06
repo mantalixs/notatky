92b15cbf2252b403d5cfcaa679de6863bf7e3cb5
Max <navrotskyimaxym@gmail.com>
2025-10-06 12:49:46 +0300

    init project with ESLint, Prettier and Husky

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import configPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  { ignores: ['node_modules/**', 'dist/**', 'build/**'] },
+
+  js.configs.recommended,   // базові правила ESLint
+  configPrettier,           // вимикає правила, що конфліктують із Prettier
+
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.node
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
+    }
+  }
+]);
