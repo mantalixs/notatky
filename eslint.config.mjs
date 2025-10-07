b321683886107e69bfb2890fef50d28d66f0843c
karidzi <karidzi123452006@gmail.com>
2025-10-07 12:04:18 +0300

    Додано правило sort-imports до ESLint конфігурації

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,21 +4,28 @@ import configPrettier from 'eslint-config-prettier';
 import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { ignores: ['node_modules/**', 'dist/**', 'build/**'] },
+    { ignores: ['node_modules/**', 'dist/**', 'build/**'] },
 
-  js.configs.recommended,   // базові правила ESLint
-  configPrettier,           // вимикає правила, що конфліктують із Prettier
+    js.configs.recommended,   // базові правила ESLint
+    configPrettier,           // вимикає правила, що конфліктують із Prettier
 
-  {
-    files: ['**/*.{js,mjs,cjs}'],
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: globals.node
-    },
-    rules: {
-      'no-unused-vars': 'warn',
-      'no-console': 'off'
+    {
+        files: ['**/*.{js,mjs,cjs}'],
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: globals.node
+        },
+        rules: {
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+            // автоматичне впорядкування імпортів
+            'sort-imports': ['warn', {
+                ignoreCase: true,
+                ignoreDeclarationSort: false,
+                ignoreMemberSort: false,
+                memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single']
+            }]
+        }
     }
-  }
 ]);
