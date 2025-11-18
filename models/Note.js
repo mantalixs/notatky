f104283c6e95e9b5c199bddd5336e23f9de93d01
karidzi <karidzi123452006@gmail.com>
2025-11-18 14:36:56 +0200

    front

--- models/Note.js
+++ models/Note.js
@@ -4,10 +4,15 @@ const NoteSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     text: { type: String, required: true },
-    tags: [{ type: String }],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    tags: [{ type: String }], // масив тегів
+    done: { type: Boolean, default: false }, // 👈 СТАН НОТАТКИ (за замовчуванням "в процесі")
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  { timestamps: true },
+  { timestamps: true }, // createdAt / updatedAt
 );
 
 module.exports = mongoose.model("Note", NoteSchema);
