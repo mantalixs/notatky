0749f13f876ee4906a9ec7a7f5d75b270d3a1d80
Max <navrotskyimaxym@gmail.com>
2025-12-11 20:32:28 +0200

    Fix Atlas connection

--- config/db.js
+++ config/db.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 
-async function connectDB() {
-  const uri = process.env.MONGO_URI || "mongodb://localhost:27017/notatky";
-
+const connectDB = async () => {
   try {
-    await mongoose.connect(uri);
-    console.log("MongoDB connected");
+    const uri = process.env.MONGO_URI;
+
+    if (!uri) {
+      throw new Error(
+        "MONGO_URI=mongodb+srv://notatky_user:Notatky12345@cluster0.dseieg2.mongodb.net/notatky?retryWrites=true&w=majority&appName=Cluster0\n",
+      );
+    }
+
+    await mongoose.connect(uri); // без зайвих опцій
+
+    console.log("✅ MongoDB connected");
   } catch (err) {
-    console.error("MongoDB connection error:", err);
+    console.error("❌ MongoDB connection error:", err);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
