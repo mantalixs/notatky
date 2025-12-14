d2d7d788afa8802124c97c3bae2ad3fac1a243fa
Max <navrotskyimaxym@gmail.com>
2025-12-14 18:06:41 +0200

    Laba 5 and 6

--- config/db.js
+++ config/db.js
@@ -1,17 +1,12 @@
 const mongoose = require("mongoose");
-
-const ATLAS_URI =
-  "mongodb+srv://notatky_user:Notatky12345@cluster0.dseieg2.mongodb.net/notatky?retryWrites=true&w=majority&appName=Cluster0";
-
 const connectDB = async () => {
   try {
     let uri;
 
-    // Якщо це Render – використовуємо завжди Atlas URI
     if (process.env.RENDER === "true") {
       uri = ATLAS_URI;
     } else {
-      // Локально – з .env, а якщо немає, тоді теж Atlas
+
       uri = process.env.MONGO_URI || ATLAS_URI;
     }
 
